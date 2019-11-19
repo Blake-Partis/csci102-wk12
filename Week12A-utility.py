764f972042999464a9db5703843ce265e31d3795
Blake <bpartis@mymail.mines.edu>
2019-11-18 18:47:32 -0700

    creaded PrintOutput

--- Week12A-utility.py
+++ Week12A-utility.py
@@ -4,13 +4,3 @@
 
 def PrintOutput(string):
     print("OUTPUT", string)
-    
-def LoadFile(file):
-    my_list = []
-    f = open(file, "r")
-    for line in f:
-        line = line.strip("\n")
-        my_list.append(line)
-    f.close
-    print("OUTPUT", my_list)
-
